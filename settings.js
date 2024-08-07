e66fdc42e311f1a81a09d1bb882c8f1c342bf521
Abdogywyw <abdoelnagar600@gmail.com>
2024-08-07 22:12:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "201012606541"
+global.ownername = "عمك النجار باشا العظيم "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '201012606541'  //creator number
+global.ownername = ' عمك النجار باشا العظيم ' //owner name
+global.botname = 'ELNGAR BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
